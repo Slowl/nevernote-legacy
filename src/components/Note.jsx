45f66e6e1ce335319b97311fcb7253d975c74d75
Slowl <hedi0475@gmail.com>
2019-10-18 16:05:32 +0200

    added component to display existing notes

---
+++ src/components/Note.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FiX } from "react-icons/fi"
+
+const NoteContainer = styled.div`
+  font-family: 'Montserrat', sans-serif;
+  width: 90%;
+  margin: 1em auto;
+  border-radius: 0 5px 5px 0;
+  border-left: 1px solid rgba(0,0,0, .3);
+  display: flex;
+
+  cursor: pointer;
+  transition: .15s;
+
+  :hover {
+    transform: translateX(2px);
+  }
+
+`
+
+const ContentContainer = styled.div`
+
+  padding: .5em;
+
+  .title {
+    padding-left: .3em;
+    font-weight: 500;
+    font-size: 1.3em;
+    word-spacing: 1px;
+    color: rgba(0,0,0, .7)
+  }
+
+  .content {
+    padding-left: .3em;
+    font-weight: 400;
+    font-size: 1.1em;
+    letter-spacing: 1px;
+    word-spacing: 1px;
+    color: rgba(0,0,0, .4);
+  }
+`
+
+const Delete = styled.div`
+  background-color: #fcdfdf;
+  color: rgba(255,255,255, 1);
+  border-radius: 0 5px 5px 0;
+  padding: 1.2em .3em 0;
+  font-size: 1.5em;
+  opacity: 0;
+  transform: translateX(-3px);
+  transition: .3s;
+
+  :hover {
+    background-color: #ffb1b1;
+  }
+
+  ${NoteContainer}:hover & {
+    transform: translateX(3px);
+    opacity: 1;
+  }
+`
+
+const Note = () => {
+  return (
+    <NoteContainer>
+      <ContentContainer>
+        <div class="title">
+          Un titre
+        </div>
+        <div class="content">
+          Simplement le debut de la note, une sorte de petit preview histoire de
+        </div>
+      </ContentContainer>
+
+      <Delete><FiX /></Delete>
+    </NoteContainer>
+  )
+}
+
+export default Note
