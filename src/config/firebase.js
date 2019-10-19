12c5ac436898473f250ddd24e0d5cedf993a5c2a
Slowl <hedi0475@gmail.com>
2019-10-19 07:36:58 +0200

    added firestore conf and pkg

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB128hpbOOvFn7pzfIxfFEcIT3hZ3cZIic",
+    authDomain: "nevern0te.firebaseapp.com",
+    databaseURL: "https://nevern0te.firebaseio.com",
+    projectId: "nevern0te",
+    storageBucket: "nevern0te.appspot.com",
+    messagingSenderId: "248058225325",
+    appId: "1:248058225325:web:ce4a8eeaf9221fe1d5dc24"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+export default firebase
