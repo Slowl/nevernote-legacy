fcd59d00fd7ae916f2aa63ea03b327cd16495b85
Slowl <hedi0475@gmail.com>
2019-11-22 16:27:01 +0100

    modified and removed Api Key from the repository

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyB128hpbOOvFn7pzfIxfFEcIT3hZ3cZIic",
+    apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "nevern0te.firebaseapp.com",
     databaseURL: "https://nevern0te.firebaseio.com",
     projectId: "nevern0te",
     storageBucket: "nevern0te.appspot.com",
     messagingSenderId: "248058225325",
-    appId: "1:248058225325:web:ce4a8eeaf9221fe1d5dc24"
+    appId: process.env.FIREBASE_APP_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
