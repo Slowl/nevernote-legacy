6d97e1c885d202ea4248e96782f78ea8e1934b07
hedi <hedi.kochad@bruce.work>
2020-12-31 18:19:00 +0100

    added auth functions and pages

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,37 @@
-import firebase from "firebase";
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+
 // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: "nevern0te.firebaseapp.com",
     databaseURL: "https://nevern0te.firebaseio.com",
     projectId: "nevern0te",
     storageBucket: "nevern0te.appspot.com",
     messagingSenderId: "248058225325",
-    appId: process.env.FIREBASE_APP_ID
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig)
 
-export default firebase
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+const googleProvider = new firebase.auth.GoogleAuthProvider()
+
+export const signInWithGoogle = () => {
+  auth.signInWithPopup(googleProvider).then((res) => {
+    console.log(res.user)
+  }).catch((error) => {
+    console.log(error.message)
+  })
+}
+
+export const logOut = () => {
+  auth.signOut().then(()=> {
+    console.log('logged out')
+  }).catch((error) => {
+    console.log(error.message)
+  })
+}
---
+++ src/pages/MainApp.jsx
@@ -0,0 +1,254 @@
+import React, { useState, useEffect, useContext } from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import { firestore, logOut } from '../config/firebase'
+import { LIGHT, DARK } from '../config/colors.js'
+import localforage from 'localforage'
+import Swipe from 'react-easy-swipe'
+import { FiChevronRight } from "react-icons/fi"
+
+import NoteNav from '../components/NoteNav'
+import NoteEditor from '../components/NoteEditor'
+import { UserContext } from "../providers/userProvider"
+import { useNavigate } from "@reach/router"
+
+
+const NotesContainer = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Hind+Madurai:wght@300;400;500&display=swap');
+  width: 100vw;
+  min-width: 100vw;
+  max-width: 100vw;
+  height: 100vh;
+  min-height: 100vh;
+  max-height: 100vh;
+  display: flex;
+  overflow: hidden;
+  background-color: ${props => props.theme.white};
+  transition: all ease .4s;
+`
+
+const SwipeIndicator = styled.div`
+  display: none;
+
+  @media screen and (max-width: 45em) {
+    z-index: 1000;
+    display: block;
+    position: absolute;
+    background-color: ${props => props.open ? `${props.theme.navbg}` : `${props.theme.white}`};
+    border-radius: 40px;
+    width: 25px;
+    height: 25px;
+    right: ${props => props.open ? `-4px` : `calc(${props.windowWidth}px - 24px)`};
+    top: 50%;
+    transform: ${props => props.open ? "rotate(180deg)" : "rotate(0deg)"};
+    transition: all .6s;
+
+    svg {
+      padding: .12em .1em .1em .25em;
+      color: ${props => props.theme.grey6};
+      font-size: 1.15em;
+    }
+  }
+`
+
+const MainApp = () => {
+
+  const navigate = useNavigate()
+  const user = useContext(UserContext);
+  const [redirectPath, setRedirectPath] = useState(null);
+  const [ title, setTitle ] = useState('')
+  const [ note, setNote ] = useState('')
+  const [ marked, setMarked ] = useState(false)
+  const [ data, setData ] = useState([])
+  const [ usedId, setUsedId ] = useState(undefined)
+  const [ done, setReqState ] = useState(false)
+  const [ action, setAction ] = useState('')
+  const [ filterIndicator, setFilterIndicator ] = useState('')
+  const [ theme, setTheme ] = useState(DARK)
+  const [ isLight, setThemeIsLight ] = useState(false)
+  const [ isSwiped, setSwipe ] = useState(true)
+  const [ windowWidth, setWindowsWidth ] = useState(0)
+
+  const db = firestore
+  const notes = db.collection('notes')
+
+  const Requestor = () => {
+    notes.get()
+    .then((querySnapshot) => {
+      let allNotes = []
+      querySnapshot.forEach((items) => {
+        const reqData = items.data()
+        allNotes.push(reqData)
+      })
+      setData(allNotes.sort((a,b) => {
+        setFilterIndicator('UPDATE')
+        return b.modifiedAt - a.modifiedAt
+      }))
+    })
+  }
+
+  useEffect(() => {
+    Requestor()
+    setWindowsWidth(window.innerWidth)
+    localforage.getItem('localTheme').then(val => {
+      val !== null && setTheme(val)
+      if(JSON.stringify(val) === JSON.stringify(DARK)){
+        setThemeIsLight(false)
+      } else {
+        setThemeIsLight(true)
+      }
+    })
+    !user && setRedirectPath("/")
+  }, [])
+
+  if (redirectPath) {
+    navigate(redirectPath)
+  }
+
+  const themeSwitcher = () => {
+    if(JSON.stringify(theme) === JSON.stringify(DARK)){
+      setTheme(LIGHT)
+      setThemeIsLight(true)
+      localforage.setItem('localTheme', LIGHT)
+    } else {
+      setTheme(DARK)
+      setThemeIsLight(false)
+      localforage.setItem('localTheme', DARK)
+    }
+  }
+
+  const filterByModifDate = () => {
+    setData([...data].sort((a,b) => {
+      setFilterIndicator('UPDATE')
+      return b.modifiedAt - a.modifiedAt
+    }))
+  }
+
+  const filterByCreaDate = () => {
+    setData([...data].sort((a,b) => {
+      setFilterIndicator('CREATION')
+      return b.createdAt - a.createdAt
+    }))
+  }
+
+  const filterByMarked = () => {
+    setData([...data].sort((a,b) => {
+      setFilterIndicator('MARKED')
+      return b.marked - a.marked
+    }))
+  }
+
+  const Resetor = () => {
+    setTitle('')
+    setNote('')
+    setUsedId(undefined)
+    onSwipeLeft()
+  }
+
+  const onSwipeRight = () => {
+    setSwipe(true)
+  }
+
+  const onSwipeLeft = () => {
+    setSwipe(false)
+  }
+
+  const handleTitleChange = e => {
+    setTitle(e.target.value.toUpperCase())
+  }
+
+  const handleMark = value => {
+    setMarked(value)
+  }
+
+  const handleNoteChange = e => {
+    setNote(e.target.value)
+  }
+
+  const sendOrUpdate = async () => {
+    if (!!title && usedId === undefined) {
+      const id = Math.random().toString(36).slice(2).padEnd(11,0)
+      handleToast('Added', false)
+      db.collection("notes").doc(id).set({id: id, title: title, note: note, marked: marked, createdAt: Date.now(), modifiedAt: Date.now() })
+      Requestor()
+      Resetor()
+      handleToast('Added', true)
+    } else if (!!usedId) {
+      handleToast('Updated', false)
+      db.collection("notes").doc(usedId).update({
+        title: title,
+        note: note,
+        marked: marked,
+        modifiedAt: Date.now()
+      })
+      .then(
+        Requestor(),
+        handleToast('Updated', true)
+      )
+    }
+  }
+
+  const handleNoteClick = (clickedTitle, clickedNote, clickedMark, clickedId) => {
+    setTitle(clickedTitle)
+    setNote(clickedNote)
+    setMarked(clickedMark)
+    setUsedId(clickedId)
+    onSwipeLeft()
+  }
+
+  const handleDeleteNote = async (deleteId) => {
+    await handleToast('Deleted', false)
+    db.collection("notes").doc(deleteId).delete()
+    .then( () => deleteId === usedId ? (Requestor(), Resetor(), handleToast('Deleted', true)) : Requestor(), handleToast('Deleted', true))
+  }
+
+  const handleToast = (text, reqState) => {
+    setAction(text)
+    setReqState(reqState)
+    setTimeout(() => {
+      setReqState(!reqState)
+    }, 1500)
+  }
+
+  return (
+      <ThemeProvider theme={theme}>
+        <Swipe
+          onSwipeLeft={onSwipeLeft}
+          onSwipeRight={onSwipeRight}
+          tolerance={100}
+        >
+          <NotesContainer>
+            <SwipeIndicator open={isSwiped} windowWidth={windowWidth}><FiChevronRight /></SwipeIndicator>
+            <NoteNav
+              data={data}
+              onNoteClick={handleNoteClick}
+              onDeleteClick={handleDeleteNote}
+              reset={() => Resetor()}
+              creationFilter={filterByCreaDate}
+              updateFilter={filterByModifDate}
+              markedFilter={filterByMarked}
+              filterValue={filterIndicator}
+              switchTheme={themeSwitcher}
+              themeValue={isLight}
+              swipe={isSwiped}
+              windowWidth={windowWidth}
+              logOut={logOut}
+            />
+            <NoteEditor
+              onTitleChange={handleTitleChange}
+              onNoteChange={handleNoteChange}
+              onClickMark={handleMark}
+              onClickSend={() => sendOrUpdate()}
+              note={note}
+              title={title}
+              marked={marked}
+              reqState={done}
+              action={action}
+            />
+          </NotesContainer>
+        </Swipe>
+
+      </ThemeProvider>
+  )
+}
+
+export default MainApp
