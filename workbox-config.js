988edb4cdf0aee6899da415521b64a2eb669bae8
hedi <hedi.kochad@bruce.work>
2021-01-01 14:25:04 +0100

    removed custom workbox config to use files from pwa-template

--- workbox-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  globDirectory: './public/',
-  globPatterns: ['\*\*/\*.{html,js}'],
-  swDest: './public/sw.js',
-  clientsClaim: true,
-  skipWaiting: true,
-  maximumFileSizeToCacheInBytes: 10000000,
-  cleanupOutdatedCaches: true,
-};
